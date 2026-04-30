e46999a62451227259eb406b8988f8c6668e2ae7
Samuel Esteves <ssamuelesteves13@gmail.com>
2026-04-30 15:14:37 +0100

    Added ClassVsStruct project from Moodle

---
+++ ClassVsStruct/PlayerClass.cs
@@ -0,0 +1,8 @@
+﻿namespace ClassVsStruct
+{
+    public class PlayerClass
+    {
+        public float Health { get; set; }
+        public float Armour { get; set; }
+    }
+}
---
+++ ClassVsStruct/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ClassVsStruct
+{
+    public class Program
+    {
+        private static void Main()
+        {
+            PlayerClass pc = new PlayerClass() { Health = 100, Armour = 100};
+            PlayerStruct ps = new PlayerStruct() { Health = 100, Armour = 100 };
+
+            KillPlayerClass(pc);
+            KillPlayerStruct(ps);
+
+            Console.WriteLine($"Class : {pc.Health}");
+            Console.WriteLine($"Struct: {ps.Health}");
+        }
+
+        private static void KillPlayerClass(PlayerClass pc)
+        {
+            pc.Health = 0;
+        }
+
+        private static void KillPlayerStruct(PlayerStruct ps)
+        {
+            ps.Health = 0;
+        }
+    }
+}
