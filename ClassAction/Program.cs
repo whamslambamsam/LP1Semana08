3f17563df2a26c1a168b6cacd0b1bd913c080aa0
Samuel Esteves <ssamuelesteves13@gmail.com>
2026-04-30 15:57:20 +0100

    Added ClassAction project

---
+++ ClassAction/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ClassAction
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello LP!");
+        }
+    }
+}
